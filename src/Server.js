c93166bff3ee08b8b229dbe3ca025f27991bc321
Alexander Emerson Batalha Carlos <133721294+Nevrand@users.noreply.github.com>
2025-10-23 15:22:04 -0300

    Fazendo alterações no server.js

--- src/Server.js
+++ src/Server.js
@@ -1,6 +1,14 @@
+require("dotenv").config(); 
 const app = require("./app");
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+  console.log(`
+  ===========================================
+  Servidor rodando com sucesso!
+  URL: http://localhost:${PORT}
+  Pressione CTRL+C para encerrar.
+  ===========================================
+  `);
 });
